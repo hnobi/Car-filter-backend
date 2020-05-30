d349ebf7e14fc5db5d06009ea7f7e80222631e03
hammed noibi <hnobi08@yahoo.com>
2020-05-30 12:41:04 +0100

    (car owners): get all car owners  - add seed data to database - add controller for get all car owners

--- server/app.js
+++ server/app.js
@@ -1,17 +1,26 @@
-const express = require( 'express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const db = require('./models');
+const { CarOwners } = db;
+
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.port;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: "Welcome" })
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Welcome" });
 });
 
+
+app.get('/cars_owners', (req, res)=>{
+  CarOwners.findAll().then(cars=>{
+    res.status(200);
+    res.send({
+      message: cars
+    })
+
+  }).catch(e => {res.send({ error: e.message });})
+  
+})
 
 app.listen(port, () => console.log(`Application runing on port ${port}`));
